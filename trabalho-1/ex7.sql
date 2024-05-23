db3b5dd4d1f47750362397dca6af0132bb5d3d05
IsaacHaRR <isaac15rosendo@gmail.com>
2024-05-22 21:27:00 -0300

    fim eu espero

--- trabalho-1/ex7.sql
+++ trabalho-1/ex7.sql
@@ -1,21 +1,46 @@
--- Exercicio : Mostrar a média de valor total por dia de locação de todas as locações confirmadas e não confirmadas, agrupadas por mês e ano.
+-- Exercicio : Mostrar a média do preço da noite por dia de locação de todas as locações confirmadas e não confirmadas, agrupadas por mês
 
-SELECT
-    TO_CHAR(data_check_in, 'YYYY')::INTEGER AS ano,
-    TO_CHAR(data_check_in, 'MM')::INTEGER AS mes,
-    ROUND(AVG(valor_total / (data_check_out - data_check_in)), 2) AS valor_medio_geral, 
-    ROUND(
-        AVG(CASE WHEN r.confirmacao = TRUE THEN valor_total / (data_check_out - data_check_in) ELSE NULL END), 
-        2
-    ) AS valor_medio_confirmados
-FROM 
-    Locacao 
-LEFT JOIN 
-    Reservar r 
-    ON Locacao.id = r.id_locacao
-GROUP BY 
-    ano, 
-    mes
-ORDER BY 
-    ano, 
-    mes;
+SELECT 
+    valores_mensais_totais.mes, 
+    preco_medio, 
+    preco_medio_confirmado
+FROM
+    (
+        SELECT
+            TO_CHAR(data_check_in, 'MM')::INTEGER AS mes,
+            ROUND(AVG (P.preco_noite), 2) as preco_medio
+        FROM
+            Locacao 
+            JOIN 
+                Reservar as R 
+                ON Locacao.id = R.id_locacao
+            JOIN
+                Propriedade as P
+                ON P.id = R.id_prop
+        GROUP BY  
+            mes
+    ) as valores_mensais_totais
+    LEFT OUTER JOIN (
+        SELECT
+            TO_CHAR(data_check_in, 'MM')::INTEGER AS mes,
+            ROUND(AVG (P.preco_noite), 2)  as preco_medio_confirmado
+        FROM
+            Locacao 
+            JOIN 
+                Reservar as R 
+                ON Locacao.id = R.id_locacao
+            JOIN
+                Propriedade as P
+                ON P.id = R.id_prop
+        WHERE
+            r.confirmacao = TRUE
+        GROUP BY  
+            mes
+    ) as valores_mensais_confirmados
+    ON (valores_mensais_confirmados.mes = valores_mensais_totais.mes)
+GROUP BY  
+    valores_mensais_totais.mes,
+    preco_medio, 
+    preco_medio_confirmado
+ORDER BY
+    valores_mensais_totais.mes ASC
